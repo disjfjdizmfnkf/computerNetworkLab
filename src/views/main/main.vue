8d32c506cbf4d9c81e507d54d4a7d9e154ca0870
disjfjdizmfnkf <3037650079@qq.com>
2024-06-25 19:56:47 +0800

    feat: main

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="main">
-    <h2>main</h2>
-    <button @click="handleExitClick">退出登录</button>
+    <div class="common-layout">
+      <el-container class="main-content">
+
+        <el-aside class="left" width="300px">
+          <div class="aside-top">
+            <button @click="handleExitClick">
+              <el-icon size="25">
+                <ArrowLeft />
+              </el-icon>
+            </button>
+            <div class="title">
+              计算机网络实验
+            </div>
+          </div>
+          <el-footer>
+
+          </el-footer>
+        </el-aside>
+
+
+        <el-main class="right">
+          Main
+        </el-main>
+      </el-container>
+    </div>
   </div>
 </template>
 
@@ -22,7 +45,31 @@ const handleExitClick = () => {
 
 
 <style scoped>
-.main {
+.main-content {
+  background-color: gray;
+  height: 100vh;
+
+  .left {
+    button {
+      border: none;
+    }
+    .aside-top {
+      height: 40px;
+      background-color: #f0f0f0;
+      display: flex;
+      align-items: center;
+      .title {
+        margin: auto;
+        color: #000;
+        text-shadow: 0 1px 0 #CCCCCC, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0,0,0,.1), 0 0 5px rgba(0,0,0,.1), 0 1px 3px rgba(0,0,0,.3), 0 3px 5px rgba(0,0,0,.2), 0 5px 10px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.2), 0 20px 20px rgba(0,0,0,.15);
+      }
+    }
+  }
+
+
+  .right {
+    background-color: antiquewhite;
+  }
 
 }
 </style>
